267d74574585d9785520852143a82ad51f44d223
sharp0802 <sharp0802.business@gmail.com>
2026-07-14 12:03:27 +0900

    feat: impl evaluative logical pixels

---
+++ crates/ui/src/unit/mod.rs
@@ -0,0 +1,92 @@
+//! A module providing several unit types.
+
+mod dp;
+mod macros;
+mod px;
+mod sp;
+
+pub use dp::*;
+pub use px::*;
+pub use sp::*;
+
+use macros::decl_unit;
+
+use crate::cfg::Context;
+use crate::impl_op;
+
+/// Creates new logical size, in [`Dp`].
+#[inline]
+#[must_use]
+pub const fn dp(v: f32) -> Vp {
+    Vp {
+        dp: Dp::new(v),
+        sp: Sp::new(0.0),
+    }
+}
+
+/// Creates new logical size, in [`Sp`].
+#[inline]
+#[must_use]
+pub const fn sp(v: f32) -> Vp {
+    Vp {
+        dp: Dp::new(0.0),
+        sp: Sp::new(v),
+    }
+}
+
+/// A composite logical size.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Vp {
+    dp: Dp,
+    sp: Sp,
+}
+
+impl Vp {
+    /// A minimum value of the size.
+    pub const MIN: Self = Self {
+        dp: Dp::MIN,
+        sp: Sp::MIN,
+    };
+
+    /// A maximum value of the size.
+    pub const MAX: Self = Self {
+        dp: Dp::MAX,
+        sp: Sp::MAX,
+    };
+}
+
+impl Unit for Vp {
+    #[inline]
+    fn px(&self, ctx: &Context) -> Px {
+        self.dp.px(ctx) + self.sp.px(ctx)
+    }
+
+    #[inline]
+    fn zero() -> Self {
+        Self {
+            dp: Dp::zero(),
+            sp: Sp::zero(),
+        }
+    }
+}
+
+impl_op!(Vp::add(self, rhs) {
+    Self {
+        dp: self.dp + rhs.dp,
+        sp: self.sp + rhs.sp,
+    }
+});
+
+impl_op!(Vp::sub(self, rhs) {
+    Self {
+        dp: self.dp - rhs.dp,
+        sp: self.sp - rhs.sp,
+    }
+});
+
+impl_op!(Vp::mul::<f32>(self, rhs) {
+    Self {
+        dp: self.dp * rhs,
+        sp: self.sp * rhs,
+    }
+});
