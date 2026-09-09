cf8c70fbd8e38666d3ade08e37327c7438c01407
sharp0802 <sharp0802.business@gmail.com>
2026-09-09 19:36:40 +0900

    fix(render): fix wgsl file paths

--- crates/render/build.rs
+++ crates/render/build.rs
@@ -2,15 +2,15 @@
 
 use std::error::Error;
 
-use wgsl_bindgen::{RustWgslTypeMap, WgslBindgenOptionBuilder, WgslTypeSerializeStrategy};
+use wgsl_bindgen::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
     WgslBindgenOptionBuilder::default()
         .workspace_root("gfx")
-        .add_entry_point("gfx/cull.wgsl")
-        .add_entry_point("gfx/draw.wgsl")
+        .add_entry_point("gfx/rect_cull.wgsl")
+        .add_entry_point("gfx/rect_draw.wgsl")
         .serialization_strategy(WgslTypeSerializeStrategy::Bytemuck)
-        .type_map(RustWgslTypeMap)
+        .type_map(NalgebraWgslTypeMap)
         .output("src/gfx.g.rs")
         .build()?
         .generate()?;
