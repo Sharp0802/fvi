f54f5fa3a53d90358e05f55d4ae591e5d90a18b3
sharp0802 <sharp0802.business@gmail.com>
2026-09-12 09:52:46 +0900

    fix(render): fix misgenerated bind group

--- crates/render/src/draw/tex.rs
+++ crates/render/src/draw/tex.rs
@@ -1,3 +1,5 @@
+use std::num::NonZero;
+
 use wgpu::*;
 
 use super::*;
@@ -33,14 +35,51 @@ impl TextureMapBind {
             .is_none_or(|(version, _)| *version != snapshot.version)
         {
             let refs: Vec<_> = snapshot.views.iter().collect();
-            let bind_group = WgpuBindGroup1::from_bindings(
-                device,
-                WgpuBindGroup1Entries::new(WgpuBindGroup1EntriesParams {
-                    texs: &refs,
-                    samp: &self.sampler,
-                }),
-            );
-            self.bind_group = Some((snapshot.version, bind_group));
+            let len: u32 = refs.len().try_into().expect("texture map too large");
+            let len_nz = NonZero::new(len).expect("texture map size cannot be zero");
+
+            let layout = device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+                label: label!("layout/{}", snapshot.version),
+                entries: &[
+                    BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: ShaderStages::FRAGMENT,
+                        ty: BindingType::Texture {
+                            sample_type: TextureSampleType::Float { filterable: true },
+                            view_dimension: TextureViewDimension::D2,
+                            multisampled: false,
+                        },
+                        // wgpu-bindgen generates None for binding_array<T>
+                        count: Some(len_nz),
+                    },
+                    BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: ShaderStages::FRAGMENT,
+                        ty: BindingType::Sampler(SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+            });
+
+            let bind_group = device.create_bind_group(&BindGroupDescriptor {
+                label: label!("bind_group/{}", snapshot.version),
+                layout: &layout,
+                entries: &[
+                    BindGroupEntry {
+                        binding: 0,
+                        resource: BindingResource::TextureViewArray(&refs),
+                    },
+                    BindGroupEntry {
+                        binding: 1,
+                        resource: BindingResource::Sampler(&self.sampler),
+                    },
+                ],
+            });
+
+            #[expect(unsafe_code, reason = "wgpu-bindgen generates invalid bind-group")]
+            let bind_group_typed = unsafe { WgpuBindGroup1::from_raw(bind_group) };
+
+            self.bind_group = Some((snapshot.version, bind_group_typed));
         }
 
         &self.bind_group.as_ref().unwrap().1
