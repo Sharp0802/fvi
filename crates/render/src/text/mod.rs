4c37ee5c5ec378940e75e121d665aa2ac59308ff
sharp0802 <sharp0802.business@gmail.com>
2026-09-21 17:29:14 +0900

    feat(render): impl glyph cache

--- crates/render/src/text/mod.rs
+++ crates/render/src/text/mod.rs
@@ -1,6 +1,7 @@
 //! A module for text system.
 
 mod atlas;
+mod cache;
 mod error;
 mod font;
 mod image;
@@ -8,9 +9,12 @@ mod raster;
 mod style;
 mod unit;
 
+use atlas::*;
+use cache::*;
 pub use error::*;
 pub use font::*;
 use image::*;
+use raster::*;
 pub use style::*;
 use unit::*;
 
